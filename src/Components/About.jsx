56e6d410d9838aee17d9ab49bd57d645c8a58ab8
Thomas Platts <thomasplatts2004@gmail.com>
2024-02-05 23:01:46 +0000

    Completed about section, fully responsive, smooth scroll

---
+++ src/Components/About.jsx
@@ -0,0 +1,17 @@
+import profile from "../profile-picture.jpg"
+
+function About() {
+  return (
+    <main className="about-wrapper flex justify-center md:flex-row flex-col-reverse" id="about">
+      <div className="bg-white md:w-80 md:ml-20 md:mt-10 md:mx-40 md:pt-5 text-center rounded-lg shadow-lg shadow-white max-w-80 mx-auto mt-14">
+        <h1 className="font-bold mb-1 pb-4">About Me</h1>
+        <p className="text-xs px-3 pb-2">Hello, my name is Thomas Platts. I am an aspiring Front-End Developer who is self-taught and always keeping up to date with the new technologies that the programming world has to offer. I was born and raised in Birmingham, UK and decided to get into Web Development since Nov. 2023. What got me into programming was my natural skills of being a great problem solver but also an efficient learner. Hope you enjoy my portfolio, feel free to contact me with any queries!</p>
+      </div>
+      <div>
+        <a target="_blank" rel="noreferrer" href="https://www.github.com/tplatts04"><img src={profile} alt="Profile img" className="md:w-52 md:mt-16 rounded-3xl border-4 border-white w-52 mx-auto mt-5" /></a>
+      </div>
+    </main>
+  )
+}
+
+export default About;
--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -1,14 +1,34 @@
+import { useState } from "react";
+
+
 function Navigation() {
+  let [time, setTime] = useState()
+
+  function getTime() {
+    const newTime = new Date().toLocaleString();
+    setTime(newTime)
+  }
+
+  setInterval(() => {
+    getTime()
+  }, 1000);
+
   return (
-    <nav className="sticky flex flex-row justify-around bg-gradient-to-r from-violet-700 to-fuchsia-600 text-white py-2 shadow-md shadow-violet-700" >
+    <nav className="navigation-bar fixed flex flex-row justify-around bg-gradient-to-r from-violet-700 to-fuchsia-600 text-white py-2 shadow-md shadow-violet-700 md:text-sm text-xxs min-w-full" >
       <div>
-        <a href="#about"><h4 className="text-white text-shadow hover:scale-110 hover:text-black hover:underline transition-transform">About Me</h4></a>
+        <a href="#about"><h4 className="text-white text-shadow hover:scale-110 hover:text-black hover:underline transition-transform mt-1">About Me</h4></a>
       </div>
       <div>
-        <a href="/"><h4 className="text-white text-shadow hover:scale-110 hover:text-black hover:underline transition-transform">Projects</h4></a>
+        <a href="/"><h4 className="text-white text-shadow hover:scale-110 hover:text-black hover:underline transition-transform mt-1">Tech Stack</h4></a>
       </div>
       <div>
-        <a href="/"><h4 className="text-white text-shadow hover:scale-110 hover:text-black hover:underline transition-transform">Contact</h4></a>
+        <a href="/"><h4 className="text-white text-shadow hover:scale-110 hover:text-black hover:underline transition-transform mt-1">Projects</h4></a>
+      </div>
+      <div>
+        <a href="/"><h4 className="text-white text-shadow hover:scale-110 hover:text-black hover:underline transition-transform mt-1">Contact</h4></a>
+      </div>
+      <div>
+        <h6 className="text-white text-shadow mt-0.5 m-0 mt-1">{time}</h6>
       </div>
     </nav>
   )
