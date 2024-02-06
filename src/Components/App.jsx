ec50e89e08903323383bcc8bb3c6e08d3373f39f
Thomas Platts <thomasplatts2004@gmail.com>
2024-02-06 19:15:25 +0000

    included tech-stack, all link to worded documentation, fully responsive

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,11 +1,14 @@
 import Navigation from "./Navigation";
 import About from "./About";
+import Tech from "./TechStack";
 
 function App() {
   return (
-    <div className="App bg-black md:scroll-smooth min-h-svh">
+    <div className="App md:scroll-smooth min-h-svh bg-black">
       <Navigation />
       <About />
+      <hr className="mt-52 border-4 border-dotted border-t-0 w-1/2 border-indigo-700 mx-auto"/>
+      <Tech />
     </div>
   );
 }
